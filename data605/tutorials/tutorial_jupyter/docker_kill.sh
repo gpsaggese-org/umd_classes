99a6f7e530f33ae002a382b0229e9c4da8b83c4f
GP Saggese <33238329+gpsaggese@users.noreply.github.com>
2026-03-11 15:34:12 -0400

    Update (#375)

--- data605/tutorials/tutorial_jupyter/docker_kill.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-GIT_ROOT=$(git rev-parse --show-toplevel)
-source $GIT_ROOT/project_template/utils.sh
-
-REPO_NAME=gpsaggese
-IMAGE_NAME=umd_data605_postgres
-
-kill_container
--- data605/tutorials/tutorial_mongodb/docker_pull.sh
+++ data605/tutorials/tutorial_mongodb/docker_pull.sh
@@ -1,9 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
+# """
+# Pull Docker container image from registry.
+#
+# This script downloads a Docker container image from the remote registry
+# to the local system.
+# """
 
+# Exit immediately if any command exits with a non-zero status.
+set -e
+
+# Print each command to stdout before executing it.
+set -x
+
+# Import the utility functions.
 GIT_ROOT=$(git rev-parse --show-toplevel)
-source $GIT_ROOT/project_template/utils.sh
+source $GIT_ROOT/class_project/project_template/utils.sh
 
-REPO_NAME=gpsaggese
-IMAGE_NAME=umd_data605_mongodb
+# Load Docker configuration variables for this script.
+get_docker_vars_script ${BASH_SOURCE[0]}
+source $DOCKER_NAME
+print_docker_vars
 
+# Pull the container image from the registry.
 pull_container_image
