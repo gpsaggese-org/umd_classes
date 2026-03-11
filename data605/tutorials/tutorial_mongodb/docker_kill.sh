c318ae8cc87c72bdbe733ac12ab7bf889b6ce983
GP Saggese <saggese@gmail.com>
2026-03-11 15:33:32 -0400

    Update

--- data605/tutorials/tutorial_mongodb/docker_kill.sh
+++ data605/tutorials/tutorial_mongodb/docker_kill.sh
@@ -1,9 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
+# """
+# Kill and remove a running Docker container.
+#
+# This script terminates a running Docker container matching the project
+# configuration and removes it.
+# """
 
+# Exit immediately if any command exits with a non-zero status.
+set -e
+
+# Print each command to stdout before executing it.
+set -x
+
+# Import the utility functions.
 GIT_ROOT=$(git rev-parse --show-toplevel)
-source $GIT_ROOT/project_template/utils.sh
+source $GIT_ROOT/class_project/project_template/utils.sh
 
-REPO_NAME=gpsaggese
-IMAGE_NAME=umd_data605_mongodb
+# Load Docker configuration variables for this script.
+get_docker_vars_script ${BASH_SOURCE[0]}
+source $DOCKER_NAME
+print_docker_vars
 
+# Kill and remove the container.
 kill_container
