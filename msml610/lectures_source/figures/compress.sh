0eb6a77e6ed078881b5c7930a25f1190ed349104
GP Saggese <saggese@gmail.com>
2026-03-17 19:50:00 -0400

    Update

--- msml610/lectures_source/figures/compress.sh
+++ msml610/lectures_source/figures/compress.sh
@@ -1,4 +1,6 @@
-\cp -f ../figures/*.png .
+#\cp -f ../figures/*.png .
+
+if [[ 0 == 1 ]]; then
 for file in *.png; do
   echo "Processing $file"
   du -h "$file"
@@ -7,3 +9,22 @@ for file in *.png; do
   du -h "$file"
   echo
 done
+fi;
+
+if [[ 1 == 1 ]]; then
+find . -maxdepth 1 -type f -name "*.png" -size +1M | while read file; do
+  echo "Processing: $file"
+
+  echo -n "Before: "
+  du -h "$file"
+
+  jpg="${file%.png}.jpg"
+  magick "$file" -background white -alpha remove -quality 85 "$jpg"
+
+  echo -n "After:  "
+  du -h "$jpg"
+
+  rm "$file"
+  echo
+done
+fi;
