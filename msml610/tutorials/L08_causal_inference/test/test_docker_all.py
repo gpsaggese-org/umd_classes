ac08a8915fd2663ff5c86e434d6fabbd2c69a7af
GP Saggese <33238329+gpsaggese@users.noreply.github.com>
2026-03-23 15:34:51 -0400

    gp_scratch_14 (#399)
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- msml610/tutorials/L08_causal_inference/test/test_docker_all.py
+++ msml610/tutorials/L08_causal_inference/test/test_docker_all.py
@@ -1,9 +1,10 @@
 """
-Run each notebook in class_project/project_template/ inside Docker using docker_cmd.sh.
+Run each notebook in msml610/tutorials/L08_causal_inference/ inside Docker
+using docker_cmd.sh.
 
 Import as:
 
-import class_project.project_template.test.test_docker_all as tptdal
+import msml610.tutorials.L08_causal_inference.test.test_docker_all as mtl08tdal
 """
 
 import logging
@@ -17,12 +18,6 @@ import helpers.hunit_test as hunitest
 _LOG = logging.getLogger(__name__)
 
 
-# TODO(gp): Factor out more
-# 1) pass __file__ instead of script_dir =
-# os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# 2) Create a TestDockerTestCase with methods calls automatically docker_build
-#    test_cmd
-
 # #############################################################################
 # Test_docker_build
 # #############################################################################
@@ -32,8 +27,6 @@ class Test_docker_build(hunitest.TestCase):
     """
     Test that docker_build.sh builds the Docker image successfully.
     """
-
-    # TODO(gp): Build container from scratch as superslow test.
 
     @pytest.mark.slow
     def test1(self) -> None:
@@ -56,6 +49,7 @@ class Test_docker_cmd(hunitest.TestCase):
     Test that docker_cmd.sh can run arbitrary shell commands inside Docker.
     """
 
+    @pytest.mark.slow
     def test1(self) -> None:
         """
         Test that docker_cmd.sh 'ls /data' runs without error.
@@ -67,33 +61,14 @@ class Test_docker_cmd(hunitest.TestCase):
 
 
 # #############################################################################
-# Test_docker_bash
-# #############################################################################
-
-
-class Test_docker_bash(hunitest.TestCase):
-    """
-    Test that docker_bash.sh can run commands piped via stdin inside Docker.
-    """
-
-    def test1(self) -> None:
-        """
-        Test that docker_bash.sh runs 'ls /git_root' and exits without error.
-        """
-        # Prepare inputs.
-        script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        # Run test.
-        hdoctest.run_docker_bash(script_dir, shell_cmd="ls /git_root")
-
-
-# #############################################################################
 # Test_docker_run_notebooks
 # #############################################################################
 
 
 class Test_docker_run_notebooks(hunitest.TestCase):
     """
-    Run each notebook in class_project/project_template/ via docker_cmd.sh.
+    Run each notebook in msml610/tutorials/L08_causal_inference/ via
+    docker_cmd.sh.
     """
 
     def _helper(self, notebook_name: str) -> None:
@@ -102,7 +77,7 @@ class Test_docker_run_notebooks(hunitest.TestCase):
         successfully.
 
         :param notebook_name: name of the notebook file relative to the
-            class_project/project_template/ directory (e.g., template.example.ipynb)
+            L08_causal_inference/ directory
         """
         # Prepare inputs.
         test_dir = os.path.dirname(os.path.abspath(__file__))
@@ -113,19 +88,19 @@ class Test_docker_run_notebooks(hunitest.TestCase):
     @pytest.mark.slow
     def test1(self) -> None:
         """
-        Test that template.example.ipynb runs without error inside Docker.
+        Test that L08_01_causal_inference.ipynb runs without error.
         """
         # Prepare inputs.
-        notebook_name = "template.example.ipynb"
+        notebook_name = "L08_01_causal_inference.ipynb"
         # Run test.
         self._helper(notebook_name)
 
     @pytest.mark.slow
     def test2(self) -> None:
         """
-        Test that template.API.ipynb runs without error inside Docker.
+        Test that L08_02_causal_inference.ipynb runs without error.
         """
         # Prepare inputs.
-        notebook_name = "template.API.ipynb"
+        notebook_name = "L08_02_causal_inference.ipynb"
         # Run test.
         self._helper(notebook_name)
