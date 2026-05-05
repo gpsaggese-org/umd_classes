00282d858587124ef7b384ef928dd1da012ef515
Delvitron1019 <lci2021015@iiitl.ac.in>
2026-05-05 11:24:16 -0400

    feat: integrate Ray Data into load_data for distributed loading

--- class_project/data605/Spring2026/projects/UmdTask464_DATA605_Spring2026_Ray_Housing_Price_Prediction/ray_utils.py
+++ class_project/data605/Spring2026/projects/UmdTask464_DATA605_Spring2026_Ray_Housing_Price_Prediction/ray_utils.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import fetch_california_housing
+import ray
+from ray import data
 
 def load_data():
-    data = fetch_california_housing(as_frame=True)
-    return data.frame
+    ray.init(ignore_reinit_error=True)
+    
+    data_raw = fetch_california_housing(as_frame=True)
+    df = data_raw.frame
+
+    ray_ds = data.from_pandas(df)
+    
+    return ray_ds.to_pandas()
