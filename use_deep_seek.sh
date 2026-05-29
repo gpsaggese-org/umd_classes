b32332812b70e0b67a34f19644f244a1a8ec2a2d
GP Saggese <saggese@gmail.com>
2026-05-29 16:37:20 -0400

    Update
    Pre-commit checks:
    All checks passed ✅
    
    Pre-commit checks:
    All checks passed ✅
    
    Pre-commit checks:
    All checks passed ✅

---
+++ use_deep_seek.sh
@@ -0,0 +1,5 @@
+export ANTHROPIC_AUTH_TOKEN=$OPENROUTER_API_KEY
+export ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek/deepseek-v4-flash
+export ANTHROPIC_DEFAULT_OPUS_MODEL=anthropic/haiku-4.5
+export ANTHROPIC_DEFAULT_SONNET_MODEL=anthropic/haiku-4.5
+export ANTHROPIC_BASE_URL=https://openrouter.ai/api
