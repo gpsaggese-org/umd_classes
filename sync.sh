ee41f966b2f623907eadbc6c61bf00d8eed4b285
GP Saggese <saggese@gmail.com>
2026-06-14 20:42:52 -0400

    Update

--- sync.sh
+++ sync.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
 TOPDIRS=(
-#    msml610/tutorials/L05_statistical_learning
-#    msml610/tutorials/L06_bayesian_networks
-#    msml610/tutorials/L07_prob_programming
-#    msml610/tutorials/L08_causal_inference
-#    msml610/tutorials/L09_kalman_filter
-#    msml610/tutorials/L09_multi_armed_bandits
-#    msml610/tutorials/L10_causal_discovery
-#    msml610/tutorials/L12_reinforcement_learning
-#    msml610/tutorials/L9x_refreshers
-#    tutorials/Asana
-#    tutorials/Autogen
-#    tutorials/Ax_Multi_Objective_Optimization
-#    tutorials/BambooAI
+    msml610/tutorials/L05_statistical_learning
+    msml610/tutorials/L06_bayesian_networks
+    msml610/tutorials/L07_prob_programming
+    msml610/tutorials/L08_causal_inference
+    msml610/tutorials/L09_kalman_filter
+    msml610/tutorials/L09_multi_armed_bandits
+    msml610/tutorials/L10_causal_discovery
+    msml610/tutorials/L12_reinforcement_learning
+    msml610/tutorials/L9x_refreshers
+    tutorials/Asana
+    tutorials/Autogen
+    tutorials/Ax_Multi_Objective_Optimization
+    tutorials/BambooAI
     tutorials/CausalML_Diabetes_Study
     tutorials/causalnex
     tutorials/crewai
@@ -48,6 +48,6 @@ TOPDIRS=(
 # Loop over Set 1: msml610/tutorials
 for dir in "${TOPDIRS[@]}"; do
     DST_DIR="$dir"
-    #create_links.py --src_dir class_project/project_template --dst_dir $DST_DIR --replace_links
-    \cp -f class_project/project_template/docker_bash.sh class_project/project_template/docker_build.sh class_project/project_template/docker_clean.sh class_project/project_template/docker_cmd.sh class_project/project_template/docker_exec.sh class_project/project_template/docker_jupyter.sh class_project/project_template/docker_push.sh class_project/project_template/run_jupyter.sh class_project/project_template/version.sh $DST_DIR
+    create_links.py --src_dir class_project/project_template --dst_dir $DST_DIR --replace_links
+    #\cp -f class_project/project_template/docker_bash.sh class_project/project_template/docker_build.sh class_project/project_template/docker_clean.sh class_project/project_template/docker_cmd.sh class_project/project_template/docker_exec.sh class_project/project_template/docker_jupyter.sh class_project/project_template/docker_push.sh class_project/project_template/run_jupyter.sh class_project/project_template/version.sh $DST_DIR
 done
