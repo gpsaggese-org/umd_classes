fb973d6a549c85fc9c392baa561f44971fb95dd8
GP Saggese <saggese@gmail.com>
2026-08-18 17:56:56 -0400

    Update
    Pre-commit checks:
    All checks passed ✅

--- class_scripts/gen_slides_test_utils.py
+++ class_scripts/gen_slides_test_utils.py
@@ -479,7 +479,7 @@ class Run_notes_to_pdf_py_TestCase(hunitest.TestCase):
             output_type_arg = "slides"
             toc_type_arg = "navigation"
             cleanup_action = "cleanup_after"
-            open_action = "open"
+            open_action = "open_pdf"
             # Build skip_action arguments.
             all_skip_actions = [cleanup_action, open_action]
             skip_action_args = " ".join(
@@ -523,7 +523,7 @@ class Run_notes_to_pdf_py_TestCase(hunitest.TestCase):
             --type=slides \
             --toc_type=navigation \
             --skip_action=cleanup_after \
-            --skip_action=open \
+            --skip_action=open_pdf \
             --no_pdf
         """
         _LOG.debug(hprint.to_str("self.COURSE_DIR"))
@@ -555,7 +555,7 @@ class Run_notes_to_pdf_py_TestCase(hunitest.TestCase):
             --type=slides \
             --toc_type=navigation \
             --skip_action=cleanup_after \
-            --skip_action=open
+            --skip_action=open_pdf
         """
         _LOG.debug(hprint.to_str("self.COURSE_DIR"))
         hdbg.dassert_ne(self.COURSE_DIR, "")
@@ -579,7 +579,7 @@ class Run_notes_to_pdf_py_TestCase(hunitest.TestCase):
             --type=slides \
             --toc_type=navigation \
             --skip_action=cleanup_after \
-            --skip_action=open
+            --skip_action=open_pdf
         """
         _LOG.debug(hprint.to_str("self.COURSE_DIR"))
         hdbg.dassert_ne(self.COURSE_DIR, "")
